f5d65e668ddad4c7f6ef6d4769ebe9cba0d15600
mark.huang <huangtao123689@gmail.com>
2021-03-16 08:58:45 +0800

    修改导入Excel空行显示为nan问题

--- utils/Excel.py
+++ utils/Excel.py
@@ -20,17 +20,15 @@ class Excel:
         """
         获取用户上传的Excel中的数据
         """
-        pf = pd.read_excel(self.filepath)
+        pf = pd.read_excel(self.filepath, keep_default_na=False)
         # 第一行数据是key
         keys = pf.columns.values
-        print(keys)
         # 获取所有数据
         all_records = []
         for i in pf.index.values:
             record = pf.loc[i, keys].astype('str').to_dict()
             all_records.append(record)
         return all_records
-
 
 # if __name__ == '__main__':
 #     excel = Excel(os.path.join(Settings.CMDB_FOLDER, "mysql模板.xlsx"))
